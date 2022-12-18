0afe086796c72a321d6dc90d2552d5804b25e44e
Quach Minh Triet <20522057@gm.uit.edu.vn>
2022-12-18 11:34:44 +0700

    img

--- mvc/views/client/cpanel/require_pro_menwo.php
+++ mvc/views/client/cpanel/require_pro_menwo.php
@@ -1,6 +1,14 @@
 <?php 
     require_once "./mvc/views/client/include/head.php";
 ?>
+<style>
+    #footer {
+        margin-top: 0px !important;
+        /* border-top: 1px solid #ccc; */
+        padding-top: 30px;
+
+    }
+</style>
 <body>
     <?php 
         require_once "./mvc/views/client/include/header.php";
